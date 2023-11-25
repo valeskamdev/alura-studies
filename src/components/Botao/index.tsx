6403250b5c545d5aacae5bca88d25b8a32eb6f1d
Valeska Marques <valeskkitty@gmail.com>
2023-11-25 15:22:40 -0300

    style: styling with sass

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './style.scss';
 
 class Botao extends React.Component {
     render() {
         return (
-            <button>Botão</button>
+            <button className="botao">
+                Botão
+            </button>
         )
     }
 }
